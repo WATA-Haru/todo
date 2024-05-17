60508eda4e87adf4640e3c725f413d02b1908827
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-05-17 15:58:42 +0900

    replace render function. I replace it to Dom handle. Render function returened `<div><ul><button><li>$input</li><button><ul><div>` directly. but the structure is interpreted as string. it was able to move but editor cannot recognize when the function name change.

--- todo.js
+++ todo.js
@@ -1,54 +1,113 @@
 "use strict"
 
-//todo, done, deletedで管理する。ゆくゆくはenumでTODO.todoや、TODO.deletedみたいな感じでtodo, done, deleted以外の名前を入れられないようにしたい
-const todos = [];
+/**
+ * Enums for todo status
+ * @readonly
+ * @enum {string}
+ */
+const StatusEnum = Object.freeze({
+    TODO: "todo",
+    DONE: "done",
+    DELETED: "deleted"
+});
 
-function add_todo_to_todos(input_value, status) {
+/**
+ * check if input_status is valid or not by using StatusEnum
+ * @param {string} input_status -the input status.
+ * @returns {boolean} True if the status is valid, otherwise false.
+ */
+function is_valid_status(input_status)
+{
+    return Object.values(StatusEnum).includes(input_status);
+}
+
+/**
+ * Todo items json
+ * @typedef {Object} Todo
+ * @property {string} id - Todo id (uuid). It is used when change indivisual todo item.
+ * @property {StatusEnum} status - todo status that defines statusEnum.
+ * @property {string} content - The description of the todo item.
+ */
+/**
+ * todos has todo's content and status value
+ * @type {Todo[]}
+ */
+const todos = [
+];
+
+/**
+ * @returns {void}
+ * @param {string} id Todo id (uuid).
+ * @param {string} input_value todo content field.
+ * @param {StatusEnum} input_status todo status filld.
+ */
+function append_todos(input_value, input_status)
+{
+    const uuid = crypto.randomUUID();
+
+    if (!is_valid_status(input_status))
+        throw new Error("Invalid status. Valid statuses are: 'todo', 'done', 'deleted'.");
     todos.push(
         {
-            status: status,
+            id: uuid,
+            status: input_status,
             content: input_value
         }
     )
 }
 
-function renderTodo(content)
+function create_element(tag, classname, innerText)
 {
-     return `<li class="todo_item">
-                <button class="is_done">check</button>
-                ${content}
-                <button class="is_deleted">del</button>
-            </li>`
+    const element = document.createElement(tag);
+    if (classname)
+        element.className = classname;
+    if (innerText)
+        element.innerHTML = innerText;
+    return element;
 }
 
-// event on click
+function create_button(classname, onclick_func, innerText)
+{    
+    const button = document.createElement("button");
+    if (classname)
+        button.className = classname;
+    if (onclick_func)
+        button.onclick = onclick_func;
+    if (innerText)
+        button.innerHTML = innerText;
+    return button;
+}
+
 function create_todo() {
     const input = document.querySelector(".user_input");
     if (!input.value)
         return ;
-    const todo_area = document.querySelector(".todo_area");
-    add_todo_to_todos(input.value, "todo");// TODO: user enum
 
-    let current = todos.at(-1).content;
-    const todo_item = document.createElement("div");
-    const todo_item_html = renderTodo(current);
-    
-    todo_item.innerHTML = todo_item_html;
-    todo_area.appendChild(todo_item);
+    //add todo to todos
+    const todo_area = document.querySelector(".todo_area");
+    append_todos(input.value, StatusEnum.TODO);
+    const current = todos.at(-1);
+
+    // create elms
+    const todo_item_wrapper = create_element("div", "todo_item_wrapper", false);
+    const todo_item = create_element("li", "todo_item", false);
+    const todo_content = create_element("span", "todo_content", current.content);
+    const done_button = create_button("done_button", done_todo, StatusEnum.DONE);
+    const delete_button = create_button("delete_button", delete_todo, StatusEnum.DELETED);
+   
+    // appendChild
+    todo_area.appendChild(todo_item_wrapper);
+    todo_item_wrapper.appendChild(todo_item);
+    todo_item.appendChild(done_button);
+    todo_item.appendChild(todo_content);
+    todo_item.appendChild(delete_button);
     input.value = '';
 }
 
-//function create_todo() {
-//    const input = document.querySelector(".user_input");
-//    if (!input.value)
-//        return ;
-//    const todo_area = document.querySelector(".todo_area");
-//    //const todo_area = document.querySelector("ul"); //ok
-//    //const todo_area = document.getElementByClass("hoge"); // bug occurs
-//    
-//    const todo_item = document.createElement("li");
-//
-//    todo_item.innerHTML = `${input.value}`;
-//    todo_area.appendChild(todo_item);
-//    input.value = '';
-//}
+function delete_todo() {
+    console.log("delete_todo!");
+}
+
+function done_todo() {
+    console.log("done_todo!")
+}
