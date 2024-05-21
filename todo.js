4476e69964561d3095fc3f93539d949eaac53489
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-05-21 14:03:02 +0900

    replace all snake_case with lowerCammelCase

--- todo.js
+++ todo.js
@@ -2,8 +2,16 @@
 
 /**
  * Enums for todo status
+ * @description 
+ *     **why is status defined as Object?** <br>
+ *     1. change and add status easily by using Object <br>
+ *     2. search easily on editor by using StatusEnum word because this code includes many todoXxx value <br>
+ *     3. prevent spell mistake <br>
+ * @typedef {Object} StatusEnum
+ * @property {string} TODO - Represents a todo status
+ * @property {string} DONE - Represents a done status
+ * @property {string} DELETED - Represents a deleted status
  * @readonly
- * @enum {string}
  */
 const StatusEnum = Object.freeze({
     TODO: "todo",
@@ -12,102 +20,89 @@ const StatusEnum = Object.freeze({
 });
 
 /**
- * check if input_status is valid or not by using StatusEnum
- * @param {string} input_status -the input status.
- * @returns {boolean} True if the status is valid, otherwise false.
- */
-function is_valid_status(input_status)
-{
-    return Object.values(StatusEnum).includes(input_status);
-}
-
-/**
- * Todo items json
- * @typedef {Object} Todo
+ * Todo items array
+ * @typedef {Object} Todo - todo object
  * @property {string} id - Todo id (uuid). It is used when change indivisual todo item.
  * @property {StatusEnum} status - todo status that defines statusEnum.
  * @property {string} content - The description of the todo item.
- */
-/**
  * todos has todo's content and status value
  * @type {Todo[]}
- */
+ *
+**/
 const todos = [
 ];
 
 /**
  * @returns {void}
  * @param {string} id Todo id (uuid).
- * @param {string} input_value todo content field.
- * @param {StatusEnum} input_status todo status filld.
+ * @param {StatusEnum} inputStatus todo status filld.
+ * @param {string} inputContent todo content field.
  */
-function append_todos(input_value, input_status)
+function appendTodos(inputContent, inputStatus)
 {
     const uuid = crypto.randomUUID();
 
-    if (!is_valid_status(input_status))
-        throw new Error("Invalid status. Valid statuses are: 'todo', 'done', 'deleted'.");
     todos.push(
         {
             id: uuid,
-            status: input_status,
-            content: input_value
+            status: inputStatus,
+            content: inputContent
         }
-    )
+    );
 }
 
-function create_element(tag, classname, innerText)
+function createElement(tag, className, innerText)
 {
     const element = document.createElement(tag);
-    if (classname)
-        element.className = classname;
+    if (className)
+        element.className = className;
     if (innerText)
         element.innerHTML = innerText;
     return element;
 }
 
-function create_button(classname, onclick_func, innerText)
+function createButton(className, onclickFunc, innerText)
 {    
     const button = document.createElement("button");
-    if (classname)
-        button.className = classname;
-    if (onclick_func)
-        button.onclick = onclick_func;
+    if (className)
+        button.className = className;
+    if (onclickFunc)
+        button.onclick = onclickFunc;
     if (innerText)
         button.innerHTML = innerText;
     return button;
 }
 
-function create_todo() {
-    const input = document.querySelector(".user_input");
+function createTodo() {
+    const input = document.querySelector(".userInput");
     if (!input.value)
         return ;
 
     //add todo to todos
-    const todo_area = document.querySelector(".todo_area");
-    append_todos(input.value, StatusEnum.TODO);
+    const todoArea = document.querySelector(".todoArea");
+    appendTodos(input.value, StatusEnum.TODO);
     const current = todos.at(-1);
 
     // create elms
-    const todo_item_wrapper = create_element("div", "todo_item_wrapper", false);
-    const todo_item = create_element("li", "todo_item", false);
-    const todo_content = create_element("span", "todo_content", current.content);
-    const done_button = create_button("done_button", done_todo, StatusEnum.DONE);
-    const delete_button = create_button("delete_button", delete_todo, StatusEnum.DELETED);
+    const todoItemWrapper = createElement("div", "todoItemWrapper", false);
+    const todoItem = createElement("li", "todoItem", false);
+    const todoContent = createElement("span", "todoContent", current.content);
+    const doneButton = createButton("doneButton", doneTodo, StatusEnum.DONE);
+    const deleteButton = createButton("deleteButton", deleteTodo, StatusEnum.DELETED);
    
     // appendChild
-    todo_area.appendChild(todo_item_wrapper);
-    todo_item_wrapper.appendChild(todo_item);
-    todo_item.appendChild(done_button);
-    todo_item.appendChild(todo_content);
-    todo_item.appendChild(delete_button);
+    todoArea.appendChild(todoItemWrapper);
+    todoItemWrapper.appendChild(todoItem);
+    todoItem.appendChild(doneButton);
+    todoItem.appendChild(todoContent);
+    todoItem.appendChild(deleteButton);
     input.value = '';
 }
 
-function delete_todo() {
-    console.log("delete_todo!");
+function deleteTodo() {
+    console.log("deleteTodo!");
 }
 
-function done_todo() {
-    console.log("done_todo!")
+function doneTodo() {
+    console.log("doneTodo!")
 }
