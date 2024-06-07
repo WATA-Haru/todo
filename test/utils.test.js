a97a068174f998e57522830b182858176e11b41c
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-06-07 12:52:01 +0900

    [update] add utils.js more testcase and test-passed

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,5 @@
-const { isStatusDone, isStatusDeleted, isStatusTodo } = require('./utils');
-const { statusEnum } = require('./statusEnum');
-const crypto = require('crypto');
+import { isStatusDone, isStatusDeleted, isStatusTodo } from '../src/js/utils.js';
+import { statusEnum }  from '../src/js/statusEnum.js';
 
 const newTask = {
     id: crypto.randomUUID(),
@@ -17,6 +16,14 @@ test('isStatusDone:', () => {
 
 		newTask.status = statusEnum.DELETED;
     expect(isStatusDone(newTask)).toBe(false);
+  
+    expect(isStatusDone(true)).toBe(false);
+    expect(isStatusDone(false)).toBe(false);
+    expect(isStatusDone(null)).toBe(false);
+    expect(isStatusDone(undefined)).toBe(false);
+    expect(isStatusDone([])).toBe(false);
+    expect(isStatusDone("aaa")).toBe(false);
+    expect(isStatusDone(1234)).toBe(false);
 });
 
 test('isStatusDeleted:', () => {
@@ -28,6 +35,14 @@ test('isStatusDeleted:', () => {
 
 		newTask.status = statusEnum.TODO;
     expect(isStatusDeleted(newTask)).toBe(false);
+
+    expect(isStatusDeleted(true)).toBe(false);
+    expect(isStatusDeleted(false)).toBe(false);
+    expect(isStatusDeleted(null)).toBe(false);
+    expect(isStatusDeleted(undefined)).toBe(false);
+    expect(isStatusDeleted([])).toBe(false);
+    expect(isStatusDeleted("aaa")).toBe(false);
+    expect(isStatusDeleted(1234)).toBe(false);
 });
 
 test('isStatusTodo:', () => {
@@ -39,4 +54,12 @@ test('isStatusTodo:', () => {
 
 		newTask.status = statusEnum.DELETED;
     expect(isStatusTodo(newTask)).toBe(false);
+
+    expect(isStatusTodo(true)).toBe(false);
+    expect(isStatusTodo(false)).toBe(false);
+    expect(isStatusTodo(null)).toBe(false);
+    expect(isStatusTodo(undefined)).toBe(false);
+    expect(isStatusTodo([])).toBe(false);
+    expect(isStatusTodo("aaa")).toBe(false);
+    expect(isStatusTodo(1234)).toBe(false);
 });
