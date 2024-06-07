0d29c4a24609d9943a3545a9a4a7e2a69cb4fee9
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-06-07 15:29:14 +0900

    [remove] remove settings not needed for pure js

--- jest.config.js
+++ jest.config.js
@@ -77,16 +77,16 @@ const config = {
   ],
 
   // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "mjs",
-    "cjs",
-    "jsx",
-    "ts",
-    "tsx",
-    "json",
-    "node"
-  ],
+  //moduleFileExtensions: [
+  //  "js",
+  //  "mjs",
+  //  "cjs",
+  //  "jsx",
+  //  "ts",
+  //  "tsx",
+  //  "json",
+  //  "node"
+  //],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {},
@@ -175,9 +175,9 @@ const config = {
 
   // A map from regular expressions to paths to transformers
 	// [add] it enables us to use ES Modules import export syntax in our tests
-  transform: {
-    "\\.[jt]sx?$": "babel-jest"
-  },
+  //transform: {
+  //  "\\.[jt]sx?$": "babel-jest"
+  //},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
